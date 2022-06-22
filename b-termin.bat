bc6848369c23edc3a5e183aab4ef1d858df84ffb
Baake Industries <102507495+baake-industries@users.noreply.github.com>
2022-06-23 00:26:21 +0200

    Starting the project.

---
+++ b-termin.bat
@@ -0,0 +1,20 @@
+cls
+@echo off
+echo B - Terminal  ver 1.4 for old Windows & MS-DOS
+echo (c) copyright 2021 Baake Industries, Inc.
+echo ~
+echo            @@@@@@
+echo        @@@@@@@@@@@@
+echo    @@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@    @@@@@@@
+echo  @@@@@@@      @@@@@@
+echo   @@@@@@   @@@@@@@@
+echo    @@@@@@@@@@@@@@@@@@@@
+echo     @@@@@@@@@@    @@@@@@@@
+echo      @@@@@@@      @@@@@@@@
+echo       @@@@@@   @@@@@@@@@@@
+echo        @@@@@@@@@@@@@@@@
+echo         @@@@@@@@@@@@
+echo          @@@@@@@@
+echo ~
+command.com
